9f7f79ab9a83c9191514ab1aabc97ef91eba65a9
Wellington Ideão <wellington.ideao@gmail.com>
2022-09-12 17:09:32 -0300

    HTTP: Instalando e configurando Ng-bootstrap e Bootstrap 5 - Módulo Cursos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
+import { CursosModule } from './cursos/cursos.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
