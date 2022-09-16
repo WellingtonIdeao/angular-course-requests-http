55a8850b634fadb56a4e87b46fd78b0e9e95afd5
Wellington Ideão <wellington.ideao@gmail.com>
2022-09-16 19:44:55 -0300

    HTTP: Capturando erros - + Erro com async

--- src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,4 +1,4 @@
-import { Observable } from 'rxjs';
+import { catchError, empty, Observable, of, Subject } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 
 import { CursosService } from '../cursos.service';
@@ -14,12 +14,51 @@ export class CursosListaComponent implements OnInit {
 
   //cursos!: ICurso[];
   cursos$!: Observable<ICurso[]>;
+  // Subject - objeto que pode emitir valores, neste caso boolean.
+  error$ =  new Subject<boolean>();
   
   constructor(private service: CursosService) { }
 
   ngOnInit(): void {
     //this.service.list().subscribe((dados: ICurso[]) => this.cursos = dados);
-    this.cursos$ = this.service.list();
+    this.onRefresh();
+  }
+
+  onRefresh(){
+    this.cursos$ = this.service.list()
+    .pipe(
+      // map()
+      // tap()
+      // switchMap()
+      catchError(error => {
+        console.error(error);
+        this.error$.next(true);
+        //return empty();
+        return of();
+      })
+    );
+    
+    /* this.service.list().subscribe(
+      dados =>{
+        console.log(dados);
+      },
+      error => {
+        this.error$.next(true);
+        console.log(error);
+      },
+      ()=>{
+        console.log('Observable completo!');
+      }        
+    );
+      
+    this.service.list()
+    .pipe(
+      catchError(error => empty())
+    ).subscribe(
+      dados =>{
+        console.log(dados);
+      }
+    ) */ 
   }
 
 
