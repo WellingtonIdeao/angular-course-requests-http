47709bbb42e6d7d9c82976339c74ad6ea9f97552
Wellington Ideão <wellington.ideao@gmail.com>
2022-09-22 13:52:34 -0300

    HTTP: Criando operador RxJS customizado

--- src/app/upload-file/upload-file/upload-file.component.ts
+++ src/app/upload-file/upload-file/upload-file.component.ts
@@ -4,6 +4,7 @@ import { HttpEvent, HttpEventType } from '@angular/common/http';
 
 import { environment } from './../../../environments/environment';
 import { UploadFileService } from './../upload-file.service';
+import { filterResponse, uploadProgress } from 'src/app/shared/rxjs-operators';
 
 @Component({
   selector: 'app-upload-file',
@@ -45,19 +46,28 @@ export class UploadFileComponent implements OnInit, OnDestroy {
   onUpload() {
     if (this.fileExist()) {
       this.sub.push(this.service.upload(this.files,`${environment.BASE_URL}/upload`)
-      .subscribe({
-        next: (event: HttpEvent<Object>) => {
-          //HttpEventType
-          console.log(event);
-          if(event.type == HttpEventType.Response){ 
-            console.log('Upload Concluído');
-          } else  if (event.type == HttpEventType.UploadProgress) {
-            const percentDone = Math.round((event.loaded * 100) / (event.total? event.total: 1));
-            console.log(`Progresso ${percentDone}%`);
-            this.progress = percentDone; 
-          }
-        }
-      }));
+      .pipe(
+        uploadProgress(progress => {
+          console.log(progress);
+          this.progress = progress;
+        }),
+        filterResponse()
+      ).subscribe(res => console.log('Upload Concluído')));
+
+
+      // .subscribe({
+      //   next: (event: HttpEvent<Object>) => {
+      //     // HttpEventType
+      //     // console.log(event);
+      //     if(event.type === HttpEventType.Response){ 
+      //       console.log('Upload Concluído');
+      //     } else  if (event.type === HttpEventType.UploadProgress) {
+      //       const percentDone = Math.round((event.loaded * 100) / (event.total? event.total: 1));
+      //     // console.log(`Progresso ${percentDone}%`);
+      //       this.progress = percentDone; 
+      //     }
+      //   }
+      // }));
     }
   }
 
